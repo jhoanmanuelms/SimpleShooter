fd8e1bf03b87d59a32600940c9f9be59b3ab0e06
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2024-06-11 10:31:41 +0100

    Shooting Particle Effects

--- Source/SimpleShooter/Gun.cpp
+++ Source/SimpleShooter/Gun.cpp
@@ -3,6 +3,7 @@
 
 #include "Gun.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AGun::AGun()
@@ -20,6 +21,7 @@ AGun::AGun()
 void AGun::PullTrigger()
 {
 	UE_LOG(LogTemp, Warning, TEXT("te di perroo"));
+	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
 }
 
 // Called when the game starts or when spawned
