03d162871d7575c324d0086d19e7b81cfe256997
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2024-07-22 14:51:24 +0100

    Setting Timer

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -7,5 +7,10 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
 	Super::PawnKilled(PawnKilled);
 
-	UE_LOG(LogTemp, Warning, TEXT("A Pawn was killed"));
+	APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
+
+	if (PlayerController != nullptr)
+	{
+		PlayerController->GameHasEnded(nullptr, false);
+	}
 }
---
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ShooterPlayerController.h"
+
+#include "TimerManager.h"
+
+void AShooterPlayerController::GameHasEnded(AActor* EndGameFocus, bool bIsWinner)
+{
+	Super::GameHasEnded(EndGameFocus, bIsWinner);
+
+	UE_LOG(LogTemp, Error, TEXT("Game Over"));
+	GetWorldTimerManager().SetTimer(RestartTimer, this, &APlayerController::RestartLevel, RestartDelay);
+}
