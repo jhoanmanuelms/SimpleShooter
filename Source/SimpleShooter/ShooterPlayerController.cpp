256ed2f21d9ea5d69942c75adfab38797e518a21
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2024-07-22 17:01:34 +0100

    Lose Screen

--- Source/SimpleShooter/ShooterPlayerController.cpp
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -2,11 +2,18 @@
 
 #include "ShooterPlayerController.h"
 
+#include "Blueprint/UserWidget.h"
 #include "TimerManager.h"
 
 void AShooterPlayerController::GameHasEnded(AActor* EndGameFocus, bool bIsWinner)
 {
 	Super::GameHasEnded(EndGameFocus, bIsWinner);
+
+	UUserWidget* LoseScreen = CreateWidget(this, LoseScreenClass);
+	if (LoseScreen != nullptr)
+	{
+		LoseScreen->AddToViewport();
+	}
 
 	UE_LOG(LogTemp, Error, TEXT("Game Over"));
 	GetWorldTimerManager().SetTimer(RestartTimer, this, &APlayerController::RestartLevel, RestartDelay);
